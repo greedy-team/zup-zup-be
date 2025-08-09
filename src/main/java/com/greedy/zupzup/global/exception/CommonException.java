d75a3ea5c5327361bd61e5b371c1f714e3640dab
chxghee <francis001021@gmail.com>
2025-08-09 14:47:20 +0900

    feat: 지원하지 않는 url에 대한 예외 핸들러 추가

--- src/main/java/com/greedy/zupzup/global/exception/CommonException.java
+++ src/main/java/com/greedy/zupzup/global/exception/CommonException.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum CommonException implements ExceptionCode {
 
+    ENDPOINT_NOT_FOUND(HttpStatus.NOT_FOUND, "요청 경로를 찾을 수 없음", "요청한 URL에 해당하는 API를 찾을 수 없습니다."),
     INVALID_REQUEST_BODY(HttpStatus.BAD_REQUEST, "잘못된 요청 본문", "요청 본문의 형식이 잘못되었습니다."),
     INVALID_QUERY_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 쿼리 파라미터", "쿼리 파라미터의 형식이 잘못되었습니다."),
     QUERY_PARAMETER_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "쿼리 파라미터 타입 불일치", "요청 파라미터의 타입이 잘못되었습니다."),
