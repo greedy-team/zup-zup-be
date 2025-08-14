5e7901399511b6023ba9d1dae77708c6a6003a6c
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-08-15 04:38:42 +0900

    feat: 서약 작성 중복 방지 [#13]

--- src/main/java/com/greedy/zupzup/quiz/presentation/dto/QuizSubmissionRequest.java
+++ src/main/java/com/greedy/zupzup/quiz/presentation/dto/QuizSubmissionRequest.java
@@ -1,10 +1,12 @@
 package com.greedy.zupzup.quiz.presentation.dto;
 
 import com.greedy.zupzup.quiz.application.dto.AnswerCommand;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public record QuizSubmissionRequest(
+        @NotEmpty(message = "퀴즈 답변을 하나 이상 제출해야 합니다.")
         List<AnswerRequest> answers
 ) {
 
