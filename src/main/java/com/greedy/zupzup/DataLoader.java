d983bf15f844aaa748a0f936f537b711ecb251c2
chxghee <francis001021@gmail.com>
2025-08-14 15:38:15 +0900

    refactor: DataLoader 메서드 분리 [#14]

--- src/main/java/com/greedy/zupzup/DataLoader.java
+++ src/main/java/com/greedy/zupzup/DataLoader.java
@@ -36,38 +36,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         // ================= 기존 구역 정보 초기화 =================
-        log.info("데이터베이스 임시 구역 정보를 초기화합니다.");
-        if (schoolAreaRepository.count() == 0) {
-            Polygon playground = geometryFactory.createPolygon(new Coordinate[]{
-                    new Coordinate(127.0752831, 37.5510817), new Coordinate(127.0742638, 37.5502523),
-                    new Coordinate(127.0749505, 37.5498356), new Coordinate(127.0759912, 37.5507372),
-                    new Coordinate(127.0752831, 37.5510817)
-            });
-            SchoolArea playgroundZone = new SchoolArea("세종대학교 운동장", playground);
-
-            Polygon sideRoad = geometryFactory.createPolygon(new Coordinate[]{
-                    new Coordinate(127.0738508, 37.5506351), new Coordinate(127.0742638, 37.5502523),
-                    new Coordinate(127.075326, 37.5511285), new Coordinate(127.0747198, 37.5516133),
-                    new Coordinate(127.0740868, 37.5510902), new Coordinate(127.0739902, 37.5510051),
-                    new Coordinate(127.0741565, 37.5508605), new Coordinate(127.0738508, 37.5506351)
-            });
-            SchoolArea sideRoadZone = new SchoolArea("세종대학교 운동장 옆 길", sideRoad);
-
-            Polygon aiCenter = geometryFactory.createPolygon(new Coordinate[]{
-                    new Coordinate(127.0754923, 37.551137), new Coordinate(127.0757605, 37.5508988),
-                    new Coordinate(127.076018, 37.5510647), new Coordinate(127.0757015, 37.5512986),
-                    new Coordinate(127.0754923, 37.551137)
-            });
-            SchoolArea aiCenterZone = new SchoolArea("세종대학교 AI 센터", aiCenter);
-
-            schoolAreaRepository.saveAll(Arrays.asList(playgroundZone, sideRoadZone, aiCenterZone));
-            log.info("개발용 임시 구역 정보 초기화 완료!");
-        } else {
-            log.info("구역 정보가 이미 존재하여 초기화를 건너뜁니다.");
-        }
-
-
+        initSchoolAreaData();
         // ================= 카테고리 및 관련 정보 초기화 =================
+        initCategoryData();
+    }
+
+    private void initCategoryData() {
         log.info("카테고리 및 관련 특성 정보를 초기화합니다.");
         if (categoryRepository.count() == 0) {
             // --- 1. 전자기기 카테고리 ---
@@ -118,6 +92,38 @@ public class DataLoader implements CommandLineRunner {
             log.info("카테고리 및 관련 특성 정보 초기화 완료!");
         } else {
             log.info("카테고리 정보가 이미 존재하여 초기화를 건너뜁니다.");
+        }
+    }
+
+    private void initSchoolAreaData() {
+        log.info("데이터베이스 임시 구역 정보를 초기화합니다.");
+        if (schoolAreaRepository.count() == 0) {
+            Polygon playground = geometryFactory.createPolygon(new Coordinate[]{
+                    new Coordinate(127.0752831, 37.5510817), new Coordinate(127.0742638, 37.5502523),
+                    new Coordinate(127.0749505, 37.5498356), new Coordinate(127.0759912, 37.5507372),
+                    new Coordinate(127.0752831, 37.5510817)
+            });
+            SchoolArea playgroundZone = new SchoolArea("세종대학교 운동장", playground);
+
+            Polygon sideRoad = geometryFactory.createPolygon(new Coordinate[]{
+                    new Coordinate(127.0738508, 37.5506351), new Coordinate(127.0742638, 37.5502523),
+                    new Coordinate(127.075326, 37.5511285), new Coordinate(127.0747198, 37.5516133),
+                    new Coordinate(127.0740868, 37.5510902), new Coordinate(127.0739902, 37.5510051),
+                    new Coordinate(127.0741565, 37.5508605), new Coordinate(127.0738508, 37.5506351)
+            });
+            SchoolArea sideRoadZone = new SchoolArea("세종대학교 운동장 옆 길", sideRoad);
+
+            Polygon aiCenter = geometryFactory.createPolygon(new Coordinate[]{
+                    new Coordinate(127.0754923, 37.551137), new Coordinate(127.0757605, 37.5508988),
+                    new Coordinate(127.076018, 37.5510647), new Coordinate(127.0757015, 37.5512986),
+                    new Coordinate(127.0754923, 37.551137)
+            });
+            SchoolArea aiCenterZone = new SchoolArea("세종대학교 AI 센터", aiCenter);
+
+            schoolAreaRepository.saveAll(Arrays.asList(playgroundZone, sideRoadZone, aiCenterZone));
+            log.info("개발용 임시 구역 정보 초기화 완료!");
+        } else {
+            log.info("구역 정보가 이미 존재하여 초기화를 건너뜁니다.");
         }
     }
 }
