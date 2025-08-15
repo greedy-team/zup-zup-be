604f2bc176822aaadf8d2d59fa5894b2071e4ff4
chxghee <francis001021@gmail.com>
2025-08-15 19:55:58 +0900

    feat: 테스트 클래스 activeProfile 설정 [#19]

--- src/test/java/com/greedy/zupzup/ZupzupApplicationTests.java
+++ src/test/java/com/greedy/zupzup/ZupzupApplicationTests.java
@@ -2,7 +2,9 @@ package com.greedy.zupzup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ZupzupApplicationTests {
 
