b23416a562d66441c0cb14b031910dd70215292c
chxghee <francis001021@gmail.com>
2025-08-15 22:50:33 +0900

    test: 서비스 단위 테스트용 추상 클래스 ServiceUnitTest 작성 [#19]

---
+++ src/test/java/com/greedy/zupzup/global/ServiceUnitTest.java
@@ -0,0 +1,38 @@
+package com.greedy.zupzup.global;
+
+import com.greedy.zupzup.category.repository.CategoryRepository;
+import com.greedy.zupzup.category.repository.FeatureOptionRepository;
+import com.greedy.zupzup.global.infrastructure.S3ImageFileManager;
+import com.greedy.zupzup.lostitem.repository.LostItemFeatureRepository;
+import com.greedy.zupzup.lostitem.repository.LostItemImageRepository;
+import com.greedy.zupzup.lostitem.repository.LostItemRepository;
+import com.greedy.zupzup.schoolarea.repository.SchoolAreaRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class ServiceUnitTest {
+
+    @Mock
+    protected LostItemRepository lostItemRepository;
+
+    @Mock
+    protected LostItemImageRepository lostItemImageRepository;
+
+    @Mock
+    protected LostItemFeatureRepository lostItemFeatureRepository;
+
+    @Mock
+    protected FeatureOptionRepository featureOptionRepository;
+
+    @Mock
+    protected SchoolAreaRepository schoolAreaRepository;
+
+    @Mock
+    protected CategoryRepository categoryRepository;
+
+    @Mock
+    protected S3ImageFileManager s3ImageFileManager;
+
+}
