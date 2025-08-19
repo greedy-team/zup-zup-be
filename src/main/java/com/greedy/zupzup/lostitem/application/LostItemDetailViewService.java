546bbfa9eef453d918f8136158ba4162dbc29ba3
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-08-19 21:52:15 +0900

    fix: getWithCategoryAndAreaById 메서드 부재로 인한 오류 수정

--- src/main/java/com/greedy/zupzup/lostitem/application/LostItemDetailViewService.java
+++ src/main/java/com/greedy/zupzup/lostitem/application/LostItemDetailViewService.java
@@ -1,17 +1,14 @@
 package com.greedy.zupzup.lostitem.application;
 
 import com.greedy.zupzup.global.exception.ApplicationException;
-import com.greedy.zupzup.global.exception.CommonException;
 import com.greedy.zupzup.lostitem.application.dto.LostItemDetailViewCommand;
 import com.greedy.zupzup.lostitem.domain.LostItem;
-import com.greedy.zupzup.lostitem.domain.LostItemImage;
 import com.greedy.zupzup.lostitem.exception.LostItemException;
 import com.greedy.zupzup.lostitem.repository.LostItemImageRepository;
 import com.greedy.zupzup.lostitem.repository.LostItemRepository;
 import com.greedy.zupzup.member.domain.Member;
 import com.greedy.zupzup.member.repository.MemberRepository;
 import com.greedy.zupzup.pledge.repository.PledgeRepository;
-import com.greedy.zupzup.quiz.domain.QuizAttempt;
 import com.greedy.zupzup.quiz.repository.QuizAttemptRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
