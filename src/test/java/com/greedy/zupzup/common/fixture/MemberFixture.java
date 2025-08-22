87571660a2f79273df5e37b6d0cfa0a0b28fe0ef
chxghee <francis001021@gmail.com>
2025-08-22 10:42:54 +0900

    feat: DB 저장용 암호화된 비밀번호를 갖는 MemberFixture 작성 [#38]

--- src/test/java/com/greedy/zupzup/common/fixture/MemberFixture.java
+++ src/test/java/com/greedy/zupzup/common/fixture/MemberFixture.java
@@ -2,6 +2,7 @@ package com.greedy.zupzup.common.fixture;
 
 import com.greedy.zupzup.member.domain.Member;
 import com.greedy.zupzup.member.domain.Role;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class MemberFixture {
 
@@ -9,6 +10,19 @@ public class MemberFixture {
         return Member.builder()
                 .name("테스트유저")
                 .studentId(123456)
+                .password("asd")
+                .role(Role.USER)
+                .build();
+    }
+
+    /**
+     * 암호화된 비밀번호가 필요한 DB 저장 및 API 인수 테스트용
+     */
+    public static Member MEMBER_WITH_ENCODED_PASSWORD(String password) {
+        return Member.builder()
+                .name("테스트유저")
+                .studentId(123456)
+                .password(BCrypt.hashpw(password, BCrypt.gensalt()))
                 .role(Role.USER)
                 .build();
     }
