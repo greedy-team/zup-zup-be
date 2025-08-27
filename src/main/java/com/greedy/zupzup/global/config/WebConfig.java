1f89a2661433caf5a8ef559bf4ee274b06928b40
chxghee <francis001021@gmail.com>
2025-08-27 21:56:16 +0900

    feat: cors 설정 추가

--- src/main/java/com/greedy/zupzup/global/config/WebConfig.java
+++ src/main/java/com/greedy/zupzup/global/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.greedy.zupzup.auth.presentation.interceptor.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    private static final String[] ALLOWED_ORIGINS = {
+            "https://api.sejong-zupzup.kr" // 메인 API 서버
+    };
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -34,5 +39,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns(ALLOWED_ORIGINS)
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3000);
     }
 }
