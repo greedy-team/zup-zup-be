440eb1b86acc0abae976896679c6785931f8ec58
chxghee <francis001021@gmail.com>
2025-09-08 16:50:10 +0900

    refactor: 구역별 분실물 개수 조회 쿼리 수정 [#77]

--- src/main/java/com/greedy/zupzup/lostitem/application/LostItemSummaryService.java
+++ src/main/java/com/greedy/zupzup/lostitem/application/LostItemSummaryService.java
@@ -16,7 +16,7 @@ public class LostItemSummaryService {
 
     @Transactional(readOnly = true)
     public List<LostItemSummaryCommand> getSummary(Long categoryId) {
-        return repository.findAreaSummaries(categoryId, LostItemStatus.REGISTERED)
+        return repository.findAreaSummaries(categoryId, LostItemStatus.REGISTERED.name())
                 .stream()
                 .map(LostItemSummaryCommand::from)
                 .toList();
