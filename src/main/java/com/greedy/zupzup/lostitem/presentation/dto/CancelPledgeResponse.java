2fbef3fdc27e26d8f38e7e69eee4d2d97f96a7a3
jeonseohee9 <seohuij354@gmail.com>
2025-11-29 14:45:55 +0900

    refactor: 정팩메 추가 [#115]

--- src/main/java/com/greedy/zupzup/lostitem/presentation/dto/CancelPledgeResponse.java
+++ src/main/java/com/greedy/zupzup/lostitem/presentation/dto/CancelPledgeResponse.java
@@ -4,4 +4,8 @@ public record CancelPledgeResponse(
         Long lostItemId,
         String status,
         String message
-) {}
+) {
+    public static CancelPledgeResponse of(Long id, String status) {
+        return new CancelPledgeResponse(id, status, "서약이 정상적으로 취소되었습니다.");
+    }
+}
--- src/main/java/com/greedy/zupzup/lostitem/presentation/dto/FoundCompleteResponse.java
+++ src/main/java/com/greedy/zupzup/lostitem/presentation/dto/FoundCompleteResponse.java
@@ -4,4 +4,8 @@ public record FoundCompleteResponse(
         Long lostItemId,
         String status,
         String message
-) {}
+) {
+    public static FoundCompleteResponse of(Long id, String status) {
+        return new FoundCompleteResponse(id, status, "습득 완료되었습니다.");
+    }
+}
